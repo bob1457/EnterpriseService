eeeb47948287c78402b73a21e8345fe2f4caee6b
bob1457 <bob.yuan@yahoo.com>
2025-03-08 17:43:53 -0800

    Add shipping service

---
+++ EnterpriseService.ShippingService.Api/EventHandlers/ShippingServiceConsumer.cs
@@ -0,0 +1,21 @@
+﻿using EnterpriseService.Messaging.Events;
+using MassTransit;
+
+namespace EnterpriseService.ShippingService.Api.EventHandlers
+{
+    public class ShippingServiceConsumer : IConsumer<ProductCreatedEvent>
+    {
+        private readonly ILogger<ShippingServiceConsumer> _logger;
+
+        public ShippingServiceConsumer(ILogger<ShippingServiceConsumer> logger)
+        {
+            _logger = logger;
+        }
+        public Task Consume(ConsumeContext<ProductCreatedEvent> context)
+        {
+            _logger.LogInformation($"Product shipped: {context.Message.Id}");
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- EnterpriseService.ShippingService.Api/Program.cs
+++ EnterpriseService.ShippingService.Api/Program.cs
@@ -1,3 +1,6 @@
+using EnterpriseService.ShippingService.Api.EventHandlers;
+using MassTransit;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,27 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddMassTransit(x =>
+{
+    var configuration = builder.Configuration;
+
+    var rabbitHost = configuration.GetValue<string>("MessageBus:Uri");
+    var username = configuration.GetValue<string>("MessageBus:Username");
+    var password = configuration.GetValue<string>("MessageBus:Password");
+
+    x.SetKebabCaseEndpointNameFormatter();
+    x.AddConsumer<ShippingServiceConsumer>(); // thi is only required for consumers
+    x.UsingRabbitMq((context, config) =>
+    {
+        config.ConfigureEndpoints(context);
+        config.Host(new Uri(rabbitHost), h =>
+        {
+            h.Username(username);
+            h.Password(password);
+        });
+    });
+});
 
 var app = builder.Build();
 
